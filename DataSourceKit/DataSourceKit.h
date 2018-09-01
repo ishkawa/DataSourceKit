0357db72fc4bf05fd1286c65de701be491a90778
Yosuke Ishikawa <y@ishkawa.org>
2018-09-02 08:15:57 +0900

    Initial Commit

---
+++ DataSourceKit/DataSourceKit.h
@@ -0,0 +1,19 @@
+//
+//  DataSourceKit.h
+//  DataSourceKit
+//
+//  Created by Yosuke Ishikawa on 2018/09/02.
+//  Copyright © 2018年 Yosuke Ishikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataSourceKit.
+FOUNDATION_EXPORT double DataSourceKitVersionNumber;
+
+//! Project version string for DataSourceKit.
+FOUNDATION_EXPORT const unsigned char DataSourceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataSourceKit/PublicHeader.h>
+
+
