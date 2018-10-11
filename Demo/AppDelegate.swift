762bc5fb556d51075fee74e357d250be37e3d9c0
Yosuke Ishikawa <y@ishkawa.org>
2018-10-11 20:31:49 +0900

    Simplify demo app

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -13,10 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .white
-        window?.rootViewController = VenueDetailViewController.makeInstance()
-        window?.makeKeyAndVisible()
         return true
     }
 }
--- Demo/ViewController/VenueDetailViewController.swift
+++ Demo/ViewController/VenueDetailViewController.swift
@@ -10,50 +10,22 @@ import UIKit
 import DataSourceKit
 
 final class VenueDetailViewController: UIViewController {
-    struct Data: CellsDeclarator {
-        var venue: Venue
-        var reviews: [Review]
-        var relatedVenues: [Venue]
-        
-        func declareCells(_ cell: (CellBinder) -> Void) {
-            cell(VenueOutlineCell.makeBinder(value: venue))
-            
-            if !reviews.isEmpty {
-                cell(SectionHeaderCell.makeBinder(value: "Reviews"))
-                for review in reviews {
-                    cell(ReviewCell.makeBinder(value: review))
-                }
-            }
-            
-            if !relatedVenues.isEmpty {
-                cell(SectionHeaderCell.makeBinder(value: "Related Venues"))
-                for relatedVenue in relatedVenues {
-                    cell(RelatedVenueCell.makeBinder(value: relatedVenue))
-                }
-            }
-        }
-    }
+    var venue = Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon")
+    var reviews = [
+        Review(authorImage: #imageLiteral(resourceName: "ishkawa"), authorName: "Yosuke Ishikawa", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        Review(authorImage: #imageLiteral(resourceName: "yamotty"), authorName: "Masatake Yamoto", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+    ]
     
-    static func makeInstance() -> VenueDetailViewController {
-        let storyboard = UIStoryboard(name: "VenueDetail", bundle: nil)
-        let viewController = storyboard.instantiateInitialViewController() as! VenueDetailViewController
-        return viewController
-    }
-    
+    var relatedVenues = [
+        Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
+        Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
+        Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
+        Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
+    ]
+
     @IBOutlet private weak var collectionView: UICollectionView!
     
     private let dataSource = CollectionViewDataSource()
-    
-    private var data: Data! {
-        didSet {
-            dataSource.cellDeclarations = data.cellDeclarations
-            collectionView.reloadData()
-        }
-    }
-    
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,18 +33,27 @@ final class VenueDetailViewController: UIViewController {
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
         collectionView.dataSource = dataSource
+        
+        dataSource.cellDeclarations = cellDeclarations
+    }
+}
 
-        data = Data(
-            venue: Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
-            reviews: [
-                Review(authorImage: #imageLiteral(resourceName: "ishkawa"), authorName: "Yosuke Ishikawa", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
-                Review(authorImage: #imageLiteral(resourceName: "yamotty"), authorName: "Masatake Yamoto", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
-            ],
-            relatedVenues: [
-                Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
-                Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
-                Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
-                Venue(photo: #imageLiteral(resourceName: "Kaminarimon_at_night"), name: "Kaminarimon"),
-            ])
+extension VenueDetailViewController: CellsDeclarator {
+    func declareCells(_ cell: (CellBinder) -> Void) {
+        cell(VenueOutlineCell.makeBinder(value: venue))
+        
+        if !reviews.isEmpty {
+            cell(SectionHeaderCell.makeBinder(value: "Reviews"))
+            for review in reviews {
+                cell(ReviewCell.makeBinder(value: review))
+            }
+        }
+        
+        if !relatedVenues.isEmpty {
+            cell(SectionHeaderCell.makeBinder(value: "Related Venues"))
+            for relatedVenue in relatedVenues {
+                cell(RelatedVenueCell.makeBinder(value: relatedVenue))
+            }
+        }
     }
 }
